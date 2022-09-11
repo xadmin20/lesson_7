45b9c56617623343fd57942a432a6d963d028d4d
Nick <74838258+xadmin20@users.noreply.github.com>
2022-09-11 14:25:09 +0300

    Задача 47
    Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+Console.WriteLine("Введите количество строк двумерного массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов двумерного массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] twoDimenArr = new double[m, n];
+Random rand = new Random();
+void PrintArray(double[,] arr) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) { Console.Write($"{arr[i, j]} "); }
+        Console.WriteLine();
+        }
+    }
+
+void RandArray(double[,] arr) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) { arr[i,j] = Convert.ToDouble(rand.Next(1, 1000)/10.0); }
+        }
+    }
+
+RandArray(twoDimenArr);
+PrintArray(twoDimenArr);
